ca363b1525f48757ef8523a6078600c507b298f2
funjoker <funjoker109@gmail.com>
2020-12-06 18:47:18 +0100

    Adjust program to our needs

---
+++ Source/WoWRegeneration/Repositories/WoW434.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace WoWRegeneration.Repositories
+{
+    public class WoW434 : WoWRepository
+    {
+        public override string GetBaseUrl()
+        {
+            return "http://dist.blizzard.com.edgesuite.net/wow-pod-retail/EU/15050.direct/";
+        }
+
+        public override string GetMFilName()
+        {
+            return "wow-15595-0C3502F50D17376754B9E9CB0109F4C5.mfil";
+        }
+    }
+}
